f8a184c1f491e4a8e6ee52d47f4dce28dcc7ac5e
jmLecias <johnmarklecias.lsd@gmail.com>
2024-09-18 01:31:39 +0800

    OAuth semi working

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, flash, render_template, Response, request, send_file, redirect, url_for, jsonify, Blueprint
+from flask import Flask, flash, render_template, Response, request, send_file, redirect, url_for, jsonify, Blueprint, session
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_cors import CORS
 from flask_migrate import Migrate
 from models import *
 import secrets
-import os
-from blueprints.auth import auth_blueprint
+from dotenv import load_dotenv
+
+from blueprints.auth import auth_blueprint, oauth
 from blueprints.face import face_blueprint
+
+# Load env variables from ENV file
+load_dotenv()
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@127.0.0.1:3306/project_square'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
-CORS(app)
+CORS(app, origins=["http://localhost:3000"])
 
 db.init_app(app) 
+oauth.init_app(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 login_manager.init_app(app)
@@ -26,6 +31,34 @@ def load_user(user_id):
 app.register_blueprint(auth_blueprint, url_prefix='/auth')
 app.register_blueprint(face_blueprint, url_prefix='/face')
 
+# @app.route('/auth/login/google')
+# def login_google():
+#     try:
+#         redirect_uri = url_for('authorize_google', _external=True)
+#         return google.authorize_redirect(redirect_uri)
+#     except Exception as e:
+#         app.logger.error(f"Error during login: {str(e)}")
+#         return "Error occured during login", 500
+    
+# @app.route('/auth/authorize/google')
+# def authorize_google():
+#     token = google.authorize_access_token()
+#     userinfo_endpoint = google.server_metadata('userinfo_endpoint')
+#     resp = google.get(userinfo_endpoint)
+#     user_info = resp.json()
+#     email = user_info['email']
+    
+#     user = Users.query.filter_by(email=email).first()
+#     if not user:
+#         user = Users(email=email)
+#         db.session.add(user)
+#         db.session.commit()
+    
+#     session['email'] = email
+#     session['oauth_token'] = token
+    
+#     return "User authorized", 200
+    
 
 @app.route('/')
 def index():
